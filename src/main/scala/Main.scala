b46898601ede22f5fa2c4a00fd03434aa866bbcd
Kimmo Eklund <kimmo@kimmoeklund.fi>
2025-01-10 13:31:09 +0200

    Simplify Routes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,17 +7,9 @@ object Main extends ZIOAppDefault:
     case class OneLive() extends One
     case class TwoLive() extends Two
     val routes = Routes(
-      Method.GET / "one" -> handler {
-        for {
-          one <- ZIO.serviceAt[One]("one")
-        } yield (Response.ok)
-      },
-      Method.GET / "two" -> handler {
-        for {
-          two <- ZIO.serviceAt[Two]("two")
-        } yield (Response.ok)
-      }
-    )
+      Method.GET / "one" -> handler(ZIO.serviceAt[One]("one").map(_ => Response.ok)),
+      Method.GET / "two" -> handler(ZIO.serviceAt[Two]("two").map(_ => Response.ok))
+    ) 
     val layerOne = ZLayer.succeed(Map[String, One]("one" -> OneLive()))
     val layerTwo = ZLayer.succeed(Map[String, Two]("two" -> TwoLive()))
     val combined = layerOne ++ layerTwo
